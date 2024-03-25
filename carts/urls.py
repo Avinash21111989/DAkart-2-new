8ac793c7b4aec8327ac961d3d6d04f8b06a9c305
Anush kumar Thati <anushrlt@Anushs-MacBook-Pro.local>
2024-03-25 14:23:12 -0500

    testing the changes

--- carts/urls.py
+++ carts/urls.py
@@ -4,4 +4,6 @@ from .import views
 urlpatterns = [
     path('',views.carts,name="cart"),
     path('add_cart/<int:product_id>/',views.add_cart,name='addcart')
+   # path('check',views.check,name="check"),
+
 ]
