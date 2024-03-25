0344f79109ef77ed920c70b0870a1bf47a5558b7
gowtami <kothurigowthami@gmail.com>
2024-03-25 16:58:48 -0700

    test git

--- carts/urls.py
+++ carts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .import views
-
+# testing main
 urlpatterns = [
     path('',views.carts,name="cart"),
     path('add_cart/<int:product_id>/',views.add_cart,name='addcart')
