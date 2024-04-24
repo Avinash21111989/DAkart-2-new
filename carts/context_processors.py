83048f823dd59b517e7c148d22197d8a93e42bed
= <=>
2024-04-24 12:03:53 +0530

    code check

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -3,8 +3,11 @@ from . models import Cart, CartItem
 def cartCounter(request):
     cart_count = 0
     try:
-        cart = Cart.objects.get(cart_id = cart_id(request))
-        cart_items = CartItem.objects.all().filter(cart=cart,is_active=True)
+        if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user,is_active=True)
+        else:        
+            cart = Cart.objects.get(cart_id = cart_id(request))
+            cart_items = CartItem.objects.all().filter(cart=cart,is_active=True)
         for cart_item in cart_items:
             cart_count += cart_item.quantity
     except Cart.DoesNotExist:
--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from store.models import Product
+from django.contrib.auth.models import  User
+
 
 # Create your models here.
 class Cart(models.Model):
@@ -10,8 +12,9 @@ class Cart(models.Model):
         return self.cart_id
 
 class CartItem(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
